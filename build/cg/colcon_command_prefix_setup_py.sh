9360a559992f2a99aa5273a43509dd445157f2d5
Gustavo674 <gstavo.gouveia@sou.inteli.edu.br>
2024-11-18 14:44:20 -0300

    Inclui a pasta cg no repositório para garantir funcionamento do mapa

---
+++ build/cg/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/gustavo/Documents/Navega-o-com-ROS/install/cg_interfaces/share/cg_interfaces/package.sh"
---
+++ build/cg_interfaces/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gustavo/Documents/Navega-o-com-ROS/cg/src/cg_interfaces
+# Build directory: /home/gustavo/Documents/Navega-o-com-ROS/build/cg_interfaces
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cg_interfaces__py")
